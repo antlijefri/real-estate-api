0466051409c848e01b911ed7867175261ed445f1
antlijefri <antlijefritam@gmail.com>
2024-04-20 12:27:19 +0530

    preview api with image eresize func

--- src/api/controllers/utility/MediaController.ts
+++ src/api/controllers/utility/MediaController.ts
@@ -1,4 +1,4 @@
-import { Get, JsonController, Param, Post, Res, UploadedFiles } from 'routing-controllers';
+import { Get, JsonController, Param, Post, QueryParam, Res, UploadedFiles } from 'routing-controllers';
 import { Service } from 'typedi';
 import { MediaService } from '../../services/MediaService';
 
@@ -11,14 +11,14 @@ export class MediaController {
         // --
     }
 
-    @Post('/upload/:path')
-    public async upload(@Res() response: any, @Param('path') path: string, @UploadedFiles('files', { required: true }) files: Array<{ mimetype: string, buffer: Buffer }>): Promise<any> {
+    @Post('/upload')
+    public async upload(@Res() response: any, @QueryParam('path') path: string, @UploadedFiles('files', { required: true }) files: Array<{ mimetype: string, buffer: Buffer }>): Promise<any> {
 
         const uploadSaveFiles = [];
 
         for (const file of files) {
             try {
-                const uploadSave = this.mediaService.uploadBuffer(file.buffer, file.mimetype, path);
+                const uploadSave = this.mediaService.uploadBuffer(file.buffer, file.mimetype, path?.length ? path : '/');
                 uploadSaveFiles.push(uploadSave);
             } catch (err: any) {
                 return response.status(400).send({
@@ -31,5 +31,27 @@ export class MediaController {
             message: `Successfully uploaded Files...!`,
             data: uploadSaveFiles,
         });
+    }
+
+    @Get('/preview/:fileName')
+    public async preview(@QueryParam('path') path: string, @QueryParam('height') height: number, @QueryParam('width') width: number, @Param('fileName') fileName: string, @Res() response: any): Promise<any> {
+        try {
+
+            const preview = await this.mediaService.preview(fileName, path, height, width);
+
+            response.writeHead(200, {
+                'Content-Type': preview.contentType,
+                'Content-Length': preview.buffer.length
+            });
+
+            return response.end(preview.buffer);
+
+        } catch (err) {
+
+            return response.status(400).send({
+                message: err,
+            });
+
+        }
     }
 }
--- src/api/services/MediaService.ts
+++ src/api/services/MediaService.ts
@@ -25,7 +25,7 @@ export class MediaService {
             throw new Error('Unable to retrive file type..!');
         }
 
-        let mediaType: string;
+        let mediaType: any;
 
         if (mediaConfig.allowedImageType.includes(ext)) {
             mediaType = MediaType.image;
@@ -38,7 +38,7 @@ export class MediaService {
         }
 
         const fileName: string = `${mediaType}_${Date.now()}.${ext}`;
-        const filePath: string = process.cwd() + '/uploads' + path;
+        const filePath: string = process.cwd() + '/uploads/' + path;
 
         if (!fs.existsSync(filePath)) {
             throw new Error('Invalid Path');
@@ -76,17 +76,50 @@ export class MediaService {
         }
 
         const fileName: string = `${mediaType}_${Date.now()}.${ext}`;
-        const filePath: string = '/uploads/' + path + '/';
+        const filePath: string = path;
 
-        if (!fs.existsSync(process.cwd() + filePath)) {
+        const filePathSanity = filePath.charAt(filePath.length - 1) == '/' ? filePath : filePath + '/';
+
+        if (!fs.existsSync(process.cwd() + '/uploads/' + filePathSanity)) {
             throw new Error('Invalid Path');
         }
 
-        fs.writeFileSync(process.cwd() + filePath + fileName, buffer);
+        fs.writeFileSync(process.cwd() + '/uploads/' + filePathSanity + fileName, buffer);
 
         return {
-            fileName: fileName,
-            filePath: filePath,
+            fileName,
+            filePath: filePathSanity,
+        };
+    }
+
+    public async preview(fileName: string, path: string, height: number, width: number): Promise<{ buffer: Buffer, contentType: string }> {
+
+        const filePathSanity = path.charAt(path.length - 1) == '/' ? path : path + '/';
+        const buffer = fs.readFileSync(process.cwd() + '/uploads/' + filePathSanity + fileName);
+        const mimeType = mime.getType(fileName.split('.')[1]);
+
+        const isImageType = mediaConfig.allowedImageType.includes(mime.getExtension(mimeType));
+
+        if (isImageType) {
+            const sharp = require('sharp');
+            const imageBuffer: Buffer = await new Promise((res, rej) => {
+                sharp(buffer)
+                    .resize(width, height)
+                    .toBuffer((err: any, buff: Buffer) => {
+                        if (err) return rej(err);
+                        return res(buff);
+                    });
+            });
+
+            return {
+                buffer: imageBuffer,
+                contentType: mimeType,
+            };
+        }
+
+        return {
+            buffer,
+            contentType: mimeType,
         };
     }
 }
